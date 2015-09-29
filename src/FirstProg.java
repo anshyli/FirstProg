a6fbacd4020c30049c8a3e96512cef8439a709c9
Anshyli <nyjrtli@gmail.com>
2015-09-29 15:33:10 -0400

    First commit

---
+++ src/FirstProg.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+//my first program
+/*
+ * It does not do much
+ */
+/**
+ * 
+ * @author GBTC440008UR
+ *
+ */
+public class FirstProg {
+
+	public static void main(String[] args) {
+		String username = System.getProperty("user.name");
+//        System.out.println( "I, " + username + ", am determined to learn how to code." );
+        System.out.println( "I, " + System.getenv("USERNAME") + ", am determined to learn how to code." );
+        DateFormat df = new SimpleDateFormat("EEE, MMM d, ''yy");
+        Date dateobj = new Date();
+        System.out.println( "Today's date is " + df.format(dateobj));	
+    }
+	
+}
