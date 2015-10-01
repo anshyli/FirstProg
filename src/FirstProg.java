9e67170af6fab4ef9d2ba72619d51e1afaac46ab
Anshyli <nyjrtli@gmail.com>
2015-10-01 14:08:42 -0400

    make use of username

--- src/FirstProg.java
+++ src/FirstProg.java
@@ -11,8 +11,8 @@ public class FirstProg {
 
 	public static void main(String[] args) {
 		String username = System.getProperty("user.name");
-//        System.out.println( "I, " + username + ", am determined to learn how to code." );
-        System.out.println( "I, " + System.getenv("USERNAME") + ", am determined to learn how to code." );
+        System.out.println( "I, " + username + ", am determined to learn how to code." );
+//        System.out.println( "I, " + System.getenv("USERNAME") + ", am determined to learn how to code." );
         DateFormat df = new SimpleDateFormat("EEE, MMM d, ''yy");
         Date dateobj = new Date();
         System.out.println( "Today's date is " + df.format(dateobj));	
