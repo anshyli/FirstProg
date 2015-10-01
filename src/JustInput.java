a71487a84b4432db259c46e8099f18a11a8c67a6
Anshyli <nyjrtli@gmail.com>
2015-10-01 14:17:34 -0400

    add new file

---
+++ src/JustInput.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+ 
+/*
+ * 
+ */
+ public class JustInput
+ {
+      public static void main( String[] args )
+      {
+    	  System.out.println(args[0]);
+    	  
+          Scanner keyboard = new Scanner(System.in);
+  
+          System.out.println( "What city is the capital of France?" );
+          keyboard.next();
+
+          System.out.println( "What is 6 multiplied by 7?" );
+          keyboard.nextInt();
+ 
+          System.out.println( "What is your favorite number between 0.0 and 1.0?" );
+          keyboard.nextDouble();
+ 
+          System.out.println( "Is there anything else you would like to tell me?" );
+          
+          
+          System.out.println(keyboard.next());
+          keyboard.close();
+      }
+ }
