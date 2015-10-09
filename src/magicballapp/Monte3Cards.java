b683528b36c7f8713e46a08d2da1f47f424fb301
Anshyli <nyjrtli@gmail.com>
2015-10-08 22:15:06 -0400

    get rid of warnings

--- src/magicballapp/Monte3Cards.java
+++ src/magicballapp/Monte3Cards.java
@@ -1,16 +1,15 @@
 package magicballapp;
 import java.text.NumberFormat;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Monte3Cards {
 	protected final static String[] cover = {"##  ##  ##",
-													 "AA  ##  ##",
-													 "##  AA  ##",
-													 "##  ##  AA"  	};
+											 "AA  ##  ##",
+											 "##  AA  ##",
+											 "##  ##  AA"  	};
 	protected final static String dcover = "1   2   3";
     protected final static double miniBet = 5.0;
     protected final static double maxWin = 500.0;
@@ -84,23 +83,21 @@ public class Monte3Cards {
 	public static void main(String[] args) {
 		int acePosition = 0;
 		int guessPosition = 0;
-		boolean correct = false;
 		double yourBet = 0.0;
-		double yourCash = 100.0;
 		
 		Monte3Cards myGame = new Monte3Cards();
 		Scanner sc = new Scanner(System.in);
 		NumberFormat currency = NumberFormat.getCurrencyInstance();
 		// create random object
 		Random r = new Random();
-		while (myGame.getCurrentCash() < maxWin && myGame.getCurrentCash() >miniLose) {
+		while (myGame.getCurrentCash() < Monte3Cards.maxWin && myGame.getCurrentCash() > Monte3Cards.miniLose) {
 				// Roll it
 				do {
 					System.out.println("Put down your bet. The minimum bet is: " + currency.format(miniBet) + 
 											"\nYou can not bet more than your have either. Your current cash amount is: " + 
 											 myGame.getCurrentCash());
 					yourBet = sc.nextDouble();
-				} while (yourBet < miniBet || yourBet > yourCash);
+				} while (yourBet < Monte3Cards.miniBet || yourBet > myGame.getCurrentCash());
 				// action
 				acePosition = 1 + r.nextInt(3);
 				System.out.println("Which one is the ace?");
@@ -110,18 +107,19 @@ public class Monte3Cards {
 				myGame.addHistoryEntry(acePosition, guessPosition);
 				if (acePosition==guessPosition) {
 					//You win
-				    System.out.println(winWord);
+				    System.out.println(Monte3Cards.winWord);
 					myGame.displayCover(acePosition);
 					myGame.setCurrentCash( myGame.getCurrentCash() + yourBet); 
 				}
 				else {
 					//you lost
-				    System.out.println(lostWord + acePosition + ".\n");
+				    System.out.println(Monte3Cards.lostWord + acePosition + ".\n");
 					myGame.displayCover(acePosition);
 					myGame.setCurrentCash( myGame.getCurrentCash() - yourBet); 
 				}
 		}
 		System.out.println("Game over and here i your play history: \n");
 		myGame.displayHistory();
+		sc.close();
 	}
 }
